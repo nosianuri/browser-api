a0fb1a557d9aea230bb4a1bbed2eb603926a149c
nosianuri <nurinosia@gmail.com>
2022-06-03 13:22:33 +0600

    set update

---
+++ explore.js
@@ -0,0 +1,9 @@
+const name = 15;
+const student = {
+    name: 'Jalil', friend: 'borsha', job: 'Business',
+    makeMovie: function (name) {
+        console.log(name);
+    }
+};
+student.makeMovie('din raat');
+document.getElementById('name');
---
+++ summary.js
@@ -0,0 +1,16 @@
+alert('');
+confirm('are you single?');
+prompt('name');
+// location.reload()
+// location.href=''
+// location.hash =
+//https://www.programming-hero.com/
+
+// history.back()
+// history.go()
+
+// cookies
+
+// sessionStorage vs localStorage
+// cookies vs sessionStorage vs localStorage
+
